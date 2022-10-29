5a8ef04f7e359f5af189b6a9379ecf0e2b274c1f
swxtz <alf4r6@gmail.com>
2022-10-29 12:40:39 -0300

    verificação de tipos estaticos

---
+++ cursos/typescript/verificacao-de-tipos-estaticos.ts
@@ -0,0 +1,18 @@
+// const userName = "Gustavo";
+
+// userName();
+
+// const user = {
+//   "name": "Gustavo Mendonça",
+//   "email": "dev.gustavomendonca@gmail.com"
+// }
+
+// console.log(user.location); // vai dar erro
+// console.log(user.name); // não vai dar erro
+
+
+function sum(a:number, b:number){
+  return a + b
+}
+
+console.log(sum(1,3))
